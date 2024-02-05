ed3ded18c6875b8a9f3d604ec495914842e99439
abdulmejidamk <abdulmejidamk@gmail.com>
2024-02-05 12:49:09 -0500

    "Fixed Scrolling Error"

--- JS/app.js
+++ JS/app.js
@@ -62,3 +62,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }, 1000);
 });
+
+// Scrolling fix
+document.querySelectorAll(".nav-list a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 110,
+        behavior: "smooth",
+      });
+    }
+  });
+});
